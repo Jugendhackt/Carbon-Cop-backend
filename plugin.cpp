b4743da82dae3c27c9605d74c3325b69cb70a270
Galaxyqasar <georgm.kunze@gmail.com>
2019-10-05 10:42:48 +0200

    fix

--- plugin.cpp
+++ plugin.cpp
@@ -1,5 +1,13 @@
 #include "plugin.hpp"
 
 HttpResponse plugin(PluginArg arg){
+	/*
+	"POST server.com/upload"
+	...
+	"username:abc"
+	"password:123"
+	"strecke:5km"
+	"co2verbrauch/km:1kg"
+*/
 	return {404, "text/plain", "my plugin"};
 }
